7225e553c48a1340e0f12c84a733420a291b51ff
Ramon Amorim <ramonamorimdev@gmail.com>
2021-05-27 09:54:34 -0300

    OnPreExport added to hook stuff before exporting the collections

--- Editor/JsonExporterDataEditor.cs
+++ Editor/JsonExporterDataEditor.cs
@@ -289,6 +289,8 @@ public class JsonExporterDataEditor : Editor
 			{
 				if( _exportation.Count > 0 )
 				{
+					JsonExporterData.OnPreExport?.Invoke();
+
 					var url = _urlField.stringValue;
 					IEventTrigger sendNext = new CallOnce( SetDataDirty );
 					for( int i = _exportation.Count - 1; i >= 0; i-- )
